8db24534a9650e6594a55817d842709a794dc254
itzDingo <78661451+ItzDingo@users.noreply.github.com>
2025-07-29 20:42:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const CONFIG = {
     },
     mines: {
         minBet: 0.1,
-        maxBet: 1000,
+        maxBet: 10000,
         minMines: 1,
         maxMines: 10,
         getGridSize: function(minesCount) {
